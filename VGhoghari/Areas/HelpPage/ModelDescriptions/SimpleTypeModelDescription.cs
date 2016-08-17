7ffd8c0189f69ac6adbd22adbbb46f75b850f99f
MeraDoctor <MeraDoctor@DESKTOP-1UH6UEV>
2016-08-17 23:29:07 +0530

    Add project files.

---
+++ VGhoghari/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace VGhoghari.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ VGhoghari/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VGhoghari.WebApiApplication" Language="C#" %>

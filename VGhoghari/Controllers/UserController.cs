9346781ddf24d4a6cf14f892c4cdd723f14510c0
Rajan Sanghvi <rajansanghvi@ymail.com>
2016-08-21 05:09:03 +0530

    Added Required References.
    Integrated with the theme

---
+++ VGhoghari/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VGhoghari.Controllers {
+  public class UserController : Controller {
+
+    /// <summary>
+    /// End point to show the User Registration Page 
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public ActionResult Register() {
+      return View();
+    }
+  }
+}
